6f4ac42155385175a891d2d7a181b528105f73ea
Yasuhiro Sekiyama <sekiyamayasuhiro@gmail.com>
2023-08-14 21:03:28 -0700

    problem05

--- problems/05-selective-map.js
+++ problems/05-selective-map.js
@@ -37,8 +37,36 @@ console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isPositive, square));
 *******************************************************************************/
 
 function selectiveMap(array, selector, mapper) {
-    // Your code here
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (selector(array[i])) {
+            result.push(mapper(array[i]));
+        } else {
+            result.push(array[i]);
+        }
+    }
+    return result;
 }
+
+function isEven(n) {
+    return n % 2 === 0;
+}
+
+function isPositive(n) {
+    return n > 0;
+}
+
+function square(n) {
+    return n * n;
+}
+
+function flipSign(n) {
+    return n * -1;
+}
+
+console.log(selectiveMap([8, 5, 10, 4], isEven, square));
+// [ 64, 5, 100, 16 ]
+
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
