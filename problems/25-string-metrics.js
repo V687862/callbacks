cbfa55d076adb2981c6c1afe24c759aaed255387
Vernon <vernon.leadbeater@gmail.com>
2023-08-16 00:56:01 -0400

    Question_25

---
+++ problems/25-string-metrics.js
@@ -0,0 +1,39 @@
+// Write a function stringMetrics that:
+
+// Returns the longest string in the array.
+// Returns the shortest string in the array.
+// Checks if the last string in the array ends with the shortest string and returns a boolean value.
+// The function should return an object with the following keys: longestString, shortestString, and endsWithShortest.
+
+// Examples:
+
+// let input1 = ["apple", "banana", "cherry", "date", "elderberry"];
+// console.log(stringMetrics(input1));
+// // {
+// //   longestString: "elderberry",
+// //   shortestString: "date",
+// //   endsWithShortest: false
+// // }
+
+// // let input2 = ["apple", "banana", "cherry", "elderberryban"];
+// // console.log(stringMetrics(input2));
+// // // {
+// // //   longestString: "elderberryban",
+// // //   shortestString: "apple",
+// // //   endsWithShortest: false
+// // // }
+
+// // let input3 = ["apple", "banana", "cherry", "ban"];
+// // console.log(stringMetrics(input3));
+// // // {
+// // //   longestString: "banana",
+// // //   shortestString: "ban",
+// // //   endsWithShortest: true
+// // // }
+// *******************************************************************************/
+
+function stringMetrics(array) { {
+    // Your code here
+}
+
+// *******************************************************************************/
